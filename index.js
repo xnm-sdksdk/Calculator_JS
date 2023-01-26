4e6a4ee34dd61deced8ac9f30d3291b2f116763d
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-26 16:50:16 +0000

    JS

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+class Calculator {
+    constructor(previousOp, currentOp) {
+    }
+}
+
+
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorsButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-clear]");
+const deleteButton = document.querySelector("[data-del]");
+const previousOp = document.querySelector("[data-previous]");
+const currentOp = document.querySelector("[data-current]");
+
+
+
+
+
+
+
+
+
