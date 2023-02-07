ee3f0720da0bd4cdab1b2381521a4b4b11e58c03
xnm-sdksdk <n.mendonca@protonmail.com>
2023-02-07 15:53:24 +0000

    JS Done

--- index.js
+++ index.js
@@ -25,27 +25,68 @@ class Calculator {
 
     chooseOperation(operation) {
         if (this.currentOp === '') return
-        if (this.previousOp === '') return
+        if (this.previousOp !== '') {
+            this.compute();
+        }
         this.operation = operation;
         this.previousOp = this.currentOp;
         this.currentOp = '';
     }
 
     compute(){
+        let computation
+        const prev = parseFloat(this.previousOp)
+        const current = parseFloat(this.currentOp)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case "+":
+                computation = prev + current
+                break;
+            case "-":
+                computation = prev - current
+                break;
+            case "*":
+                computation = prev * current
+                break;
+            case "/":
+                computation = prev / current
+                break;
+        
+            default:
+                return
+        }
+        this.currentOp = computation
+        this.operation = undefined
+        this.previousOp = ''
+    }
 
+    getNumber(number) {
+        const string = number.toString()
+        const int = parseFloat(string.split('.')[0])
+        const dec = string.split('.')[1]
+        let intDisplay
+        if (isNaN(int)) {
+            intDisplay = ''
+        } else {
+            intDisplay = int.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+
+        if (dec != null) {
+            return `${intDisplay}.${dec}`
+        } else {
+            return intDisplay
+        }
     }
 
     updateDisplay(){
-        this.currentOpTextElement.innerText = this.currentOp;
-        this.previousOpTextElement.innerText = this.previousOp;
+        this.currentOpTextElement.innerText = this.getNumber(this.currentOp)
+        if (this.operation != null) {
+            this.previousOpTextElement.innerText = this.previousOp
+            `${this.getNumber(this.previousOp)} ${this.operation}`
+        } else {
+            this.previousOpTextElement.innerText = ''
+        }
     }
-
-
-
-
-
-
-
 
 }
 
@@ -56,19 +97,18 @@ const operatorsButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
 const clearButton = document.querySelector("[data-clear]");
 const deleteButton = document.querySelector("[data-del]");
-const previousOp = document.querySelector("[data-previous]");
-const currentOp = document.querySelector("[data-current]");
+const previousOpTextElement = document.querySelector("[data-previous]");
+const currentOpTextElement = document.querySelector("[data-current]");
 
 
 
-const calculator = new Calculator(previousOp, currentOp)
+const calculator = new Calculator(previousOpTextElement, currentOpTextElement)
 
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
         calculator.addNumber(button.innerText);
         calculator.updateDisplay();
     })
-
 })
 
 operatorsButtons.forEach(button => {
@@ -76,10 +116,23 @@ operatorsButtons.forEach(button => {
         calculator.chooseOperation(button.innerText);
         calculator.updateDisplay();
     })
-
 })
 
 
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
 
 
+clearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
 
